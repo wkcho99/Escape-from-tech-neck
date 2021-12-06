af29ed7c47d58d3d581700c3f8dca9ab88f6bc19
s194622 <s194622@student.dtu.dk>
2021-12-06 22:00:14 +0900

    input from user in progress

--- frontend/src/components/step1/step1.js
+++ frontend/src/components/step1/step1.js
@@ -2,6 +2,48 @@ import React, { useState } from "react";
 import {Link, BrowserRouter as Router} from 'react-router-dom';
 import "./step1.css"
 const Step1 = () => {
+  const [sensitivity, setSensitivity] = useState('')
+  const [position, setPosition] = useState('')
+  var sensitivityRadio = document.getElementsByName('sensitivity');
+  var positionRadio = document.getElementsByName('position');
+  var sensitivityChoice = ""
+  var positionChoice = ""
+  var state = {sensitivityChoice: "",
+               positionChoice: ""};
+
+  function getValuesFromUser() {
+    for (var i = 0; i < sensitivityRadio.length; i++) {       
+        if (sensitivityRadio[i].checked) {
+            sensitivityChoice = sensitivityRadio[i].value;
+            break;
+        }
+    }
+    for (var i = 0; i < positionRadio.length; i++) {       
+      if (positionRadio[i].checked) {
+        positionChoice = positionRadio[i].value;
+          break;
+      }
+    }
+  }
+
+  const sendOptions = (body) => {
+    return fetch(`http://localhost:5000/options`,{
+        'method':'POST',
+         headers : {
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify(body)
+    })
+      // .then(response => response.json())
+      // .catch(error => console.log(error))
+  }
+
+  // const insertArticle = () =>{
+  //   APIService.sendOptions({title,body})
+  //   .then((response) => props.insertedArticle(response))
+  //   .catch(error => console.log('error',error))
+  // }
+
     return (
         //<Router>
         <div className="start">
@@ -15,15 +57,15 @@ const Step1 = () => {
             <div className = "option"> 
                 Sensitivity
                 <div className = "options">
-                <div className = "high">
-                    <input type = "radio" name = "sensitivity" value="high" /> High
-                </div> 
-                <div className = "middle">
-                    <input type = "radio" name = "sensitivity" value="middle" /> Middle
-                </div> 
-                <div className = "low">
-                    <input type = "radio" name = "sensitivity" value="low" /> Low
-                </div> 
+                  <div className = "high">
+                      <input type = "radio" name = "sensitivity" value="high" /> High
+                  </div> 
+                  <div className = "middle">
+                      <input type = "radio" name = "sensitivity" value="middle" /> Middle
+                  </div> 
+                  <div className = "low">
+                      <input type = "radio" name = "sensitivity" value="low" /> Low
+                  </div> 
                 </div>
             </div>
             <div className = "option">
@@ -37,9 +79,9 @@ const Step1 = () => {
                 </div> 
                 </div>
             </div>
-            <Link to = "/start/step2">
-                <input type = "button" className = "next" value="Next >>"/>
-            </Link>
+            {/* <Link to = "/start/step2"> */}
+                <input type = "button" className = "next" value="Next >>" onclick="getValuesFromUser()"/>
+            {/* </Link> */}
             </div>
         </div>
         //</Router>
